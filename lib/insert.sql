718404b3e6bf32895e99acbe1ec93d24ad0ba3c0
homeira kalim <homeirakalim@gmail.com>
2025-03-26 21:33:13 -0400

    sql python organizing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Brown", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Cinnamon", "Curious", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 22, "Blonde", "F", "Shy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 19, "Gray", "M", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "Black", "F", "Protective", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, "8", "Dark Brown","M", "Gentle", 1);
